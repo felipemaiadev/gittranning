c9dfb9ea66a7c0fec2b2aadb7ef06c8c66a2af4f
felipemaia <felipe.maia.rj@gmai.com>
2024-12-18 04:36:49 -0300

    novas modificacoes migradas diretamente via checkout

--- index.js
+++ index.js
@@ -4,5 +4,8 @@ console.warn(`Somente o Felipe esta trabalhando nesse arquivo`)
 
 const usuario = { Nome: "Felipe Maia", Empresa: "Maia.IO" }
 
+// Esta commitado errado no branch
 const novoUsario = { Nome: "Julia Mendes", Empresa: "Julia Malls" }
 
+// Sera migrada via checkout -b => diretamente para novo branch
+const maisUmNovoUsario = { Nome: "Peter Parker", Empresa: "Spider Solutins LTDA" }
