f121ceffc8f603d59d2487d96bb48ff6df5b7177
felipemaia <felipe.maia.rj@gmai.com>
2024-12-18 05:16:58 -0300

    ajustes para novo usuario

--- index.js
+++ index.js
@@ -6,3 +6,8 @@ const usuario = { Nome: "Felipe Maia", Empresa: "Maia.IO" }
 
 const novoUsario = { Nome: "Julia Mendes", Empresa: "Julia Malls" }
 
+// Sera migrada via checkout -b => diretamente para novo branch
+const maisUmNovoUsario = { Nome: "Peter Parker", Empresa: "Spider Solutins LTDA" }
+
+// Sera migrada via stash evitando conflito com o branch de origem
+const userViaStash = { Nome: "Ze do Stash", Empresa: "StashME LTDA" }
