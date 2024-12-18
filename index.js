f2a1590b86c7b622d7951f21aa688f1fd6edb1b8
felipemaia <felipe.maia.rj@gmai.com>
2024-12-18 04:26:44 -0300

    modificacoes nn stasheadas e commitas errado no branch

--- index.js
+++ index.js
@@ -2,4 +2,7 @@ console.log(`Inicio do projeto - essa feature sera para mostrar usuarios`)
 
 console.warn(`Somente o Felipe esta trabalhando nesse arquivo`)
 
-const usuario = { Nome: "Felipe Maia", Empresa: "AutoGlass" }
+const usuario = { Nome: "Felipe Maia", Empresa: "Maia.IO" }
+
+const novoUsario = { Nome: "Julia Mendes", Empresa: "Julia Malls" }
+
